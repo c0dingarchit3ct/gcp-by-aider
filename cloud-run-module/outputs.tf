c5ec1ef3426439c947fa215d499c93a8e166bc61
Ahmed M1 <c0dingarchit3ct@gmail.com>
2024-10-20 23:04:10 -0400

    added outputs

--- cloud-run-module/outputs.tf
+++ cloud-run-module/outputs.tf
@@ -1,1 +1,19 @@
+output "service_name" {
+  value       = google_cloud_run_service.service.name
+  description = "Name of the created service"
+}
 
+output "service_revision" {
+  value       = google_cloud_run_service.service.status[0].latest_ready_revision_name
+  description = "Deployed revision for the service"
+}
+
+output "service_url" {
+  value       = google_cloud_run_service.service.status[0].url
+  description = "The URL on which the deployed service is available"
+}
+
+output "service_id" {
+  value       = google_cloud_run_service.service.id
+  description = "The ID of the deployed service"
+}
--- outputs.tf
+++ outputs.tf
@@ -29,3 +29,23 @@ output "workload_identity_pool_provider_output_id" {
   description = "Name of the Workload Identity Pool Provider"
   value       = module.gcp_github_docker.workload_identity_pool_provider_output_id
 }
+
+output "cloud_run_service_url" {
+  description = "URL of the Cloud Run service"
+  value       = module.cloud_run_service.service_url
+}
+
+output "cloud_run_service_name" {
+  description = "Name of the Cloud Run service"
+  value       = module.cloud_run_service.service_name
+}
+
+output "cloud_run_service_revision" {
+  description = "Revision of the Cloud Run service"
+  value       = module.cloud_run_service.service_revision
+}
+
+output "cloud_run_service_id" {
+  description = "ID of the Cloud Run service"
+  value       = module.cloud_run_service.service_id
+}
